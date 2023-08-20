43fad34c20c78e660ab1921f42133c9210f37ad3
jieun <najee0610@naver.com>
2023-08-20 18:39:12 +0900

    8월18일 문제추가

---
+++ goorm/230818.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+newArr=[]
+
+for i in a:
+	binary_num = bin(i)[2:]
+	count = 0
+	for j in binary_num:
+		if(j=='1'):
+			count=count+1
+	newArr.append([count, i])
+	
+newArr.sort(reverse=True)
+print(newArr[k-1][1])
