b1f629a5221ba90e826ce2bdd028ccaf55a8613e
jieun <najee0610@naver.com>
2023-08-21 22:13:17 +0900

    8월21일문제수정

--- goorm/230821.py
+++ goorm/230821.py
@@ -5,41 +5,36 @@ for i in s:
 	arr.append(i)
 
 result = []
-three_result=[]
 for i in range(1,n + 1):
     for j in range(1,n + 1):
         for k in range(1,n + 1):
             if i + j + k == n:
-                result.append(arr[:i])
-                result.append(arr[i:i+j])
-                result.append(arr[i+j:])
-for i in range(0,len(result),3):
-	three_result.append(result[i:i+3])
-print('세개씩, three_result:',three_result)
+                result.append([arr[:i], arr[i:i+j], arr[i+j:]])
 
-unique_result = []
+								
+# print('만들 수 있는 모든 배열: result',result)
 
-for i in result:
-	if i not in unique_result:
-		unique_result.append(i)
+unique_list = []
+for sub_list in result:
+    for item in sub_list:
+          if item not in unique_list:
+                unique_list.append(item)
+                unique_list.sort()
 
-sorted_result = sorted(unique_result)
-print('정렬 결과 점수, sorted_result:',sorted_result)
+# print(unique_list)
+# 중복 제거하고 sort
 
-max_total = 0
-for inner_list in three_result:
-    sum_list = []
-    for item in inner_list:
-        s_item = "".join(item)
-        for s_result in sorted_result:
-            if s_item == "".join(s_result):
-                index_sum = sorted_result.index(s_result) + 1
-                sum_list.append(index_sum)
-    
-    if len(sum_list) >= 3:
-        for i in range(len(sum_list) - 2):
-            total = sum_list[i] + sum_list[i+1] + sum_list[i+2]
-            max_total = max(max_total, total)
+sum_list = []
 
-print(max_total)
+for sub_list in result:
+    for item in sub_list:
+        if item in unique_list:
+            index = unique_list.index(item)
+            sum_list.append(index+1)
 
+# print("result2의 인덱스가 더해진 sum_list:", sum_list)
+
+grouped_sums = [sum(sum_list[i:i+3]) for i in range(0, len(sum_list), 3)]
+max_sum = max(grouped_sums)
+
+print(max_sum)
