17c9d795a550cb29fe458e50a6af6398165c4f3a
jieun <najee0610@naver.com>
2023-09-02 00:57:14 +0900

    1463 수정

--- Baekjoon/DP/1463.py
+++ Baekjoon/DP/1463.py
@@ -1,16 +1,16 @@
 n = int(input())
 dp = [0]*(n+1)
 
-for i in range(2,n+1):
+for i in range(2, n+1):
     # 일단 1을 빼는 경우로 최솟값 세팅
-    dp[i]=dp[i-1]+1
+    dp[i] = dp[i-1] + 1
 
     # 2로 나누어 떨어지는 경우 체크
-    if i%2==0:
-        dp[i]=min(dp[i],dp[i//2]+1)
+    if i%2 == 0:
+        dp[i] = min(dp[i], dp[i//2]+1)
 
     # 3으로 나누어 떨어지는 경우 체크
-    if i%3==0:
-        dp[i]=min(dp[i],dp[i//3]+1)
+    if i%3 == 0:
+        dp[i] = min(dp[i], dp[i//3]+1)
 print(dp[n])
     
