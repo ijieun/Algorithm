51d5ae1cb2b498e840220ac23d842ec48073ccb6
jieun <najee0610@naver.com>
2023-09-08 16:45:16 +0900

    solve: 10989

---
+++ Baekjoon/정렬/10989.py
@@ -0,0 +1,13 @@
+# 계수 정렬을 이용하기
+import sys
+input = sys.stdin.readline
+n = int(input())
+arr = [0]*(10000+1)
+
+for _ in range(n):
+    arr[int(input())] += 1
+
+for i in range(len(arr)):
+    if arr[i]!=0:
+        for _ in range(arr[i]):
+            print(i)
