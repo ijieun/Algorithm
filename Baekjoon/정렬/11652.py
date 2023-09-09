ca6d2213f95d6157fe0d85533ec7eddac2b0e1fe
jieun <najee0610@naver.com>
2023-09-09 21:33:02 +0900

    edit: 11652

--- Baekjoon/정렬/11652.py
+++ Baekjoon/정렬/11652.py
@@ -1,20 +1,17 @@
-# 계수 정렬로 풀기
+# 딕셔너리로 풀기
 # 1. 입력받기
 # 2. 해당 값에 있으면 위치에 +1씩 하기
 # 3. max 값의 숫자를 출력
+
 import sys
 input = sys.stdin.readline
+
+card_dict = {}
 n = int(input())
-arr = [0]*(n+1)
-
 for _ in range(n):
-    arr[int(input())] +=1
-
-max_j=0
-for _ in range(len(arr)):
-    for j in arr:
-        if j>max_j:
-            max_j=j
-        if j==max_j:
-            max_j=min(j, max_j)
-print(arr.index(max_j))
+    card = int(input())
+    if card in card_dict:
+        card_dict[card] += 1
+    else:
+        card_dict[card] =1
+print(sorted(card_dict.items(), key=lambda x:(-x[1], x[0]))[0][0])
