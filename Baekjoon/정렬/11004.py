171bcacb07b550330f2c1e22226657147da3f4c3
jieun <najee0610@naver.com>
2023-09-12 15:40:03 +0900

    solve: 11004

--- Baekjoon/정렬/11004.py
+++ Baekjoon/정렬/11004.py
@@ -1,6 +1,5 @@
 import sys
 input = sys.stdin.readline
 n, k = input().split()
-for _ in range(int(n)):
-    a = list(map(input().split()))
-print(sorted(int(a))[int(k)])
+a = list(map(int, input().split()))
+print(sorted(a)[int(k)-1])
