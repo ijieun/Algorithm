d4195f4c5f527147b86084c82f3f12a0c803eab8
jieun <najee0610@naver.com>
2023-11-02 01:12:33 +0900

    solve: 2839

--- Baekjoon/DP/2839.py
+++ Baekjoon/DP/2839.py
@@ -1,14 +1,14 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-dp = [0]*(n+1)
-for i in range(3,n+1):
-    dp[1]=0
-    dp[2]=0
+dp = [-1]*5001
+dp[3]=1
+dp[5]=1
+for i in range(6,n+1):
     if i%5 == 0:
-        dp[i]=dp[i//5]+1
+        dp[i]=dp[i-5]+1
     elif i%3 == 0:
-        dp[i]=min(dp[i], dp[i//3]+1)
-    else:
-        dp[i]=-1
+        dp[i]=dp[i-3]+1
+    elif dp[i-3]>0 and dp[i-5]>0:
+        dp[i]=min(dp[i-3], dp[i-5])+1
 print(dp[n])
