c98b0d143db034eeecef53185562b752c4a4261e
jieun <najee0610@naver.com>
2023-11-05 00:01:43 +0900

    solve: 1149

--- Baekjoon/DP/2839.py
+++ Baekjoon/DP/2839.py
@@ -12,3 +12,4 @@ for i in range(6,n+1):
     elif dp[i-3]>0 and dp[i-5]>0:
         dp[i]=min(dp[i-3], dp[i-5])+1
 print(dp[n])
+# https://velog.io/@y7y1h13/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%EB%B0%B1%EC%A4%80-2839%EB%B2%88-%EC%84%A4%ED%83%95-%EB%B0%B0%EB%8B%ACdppython
