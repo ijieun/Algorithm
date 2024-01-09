fc86747dc9459ac045f03a16098b4a3173bc0300
jieun <najee0610@naver.com>
2024-01-09 15:25:03 +0900

    2231.py

---
+++ Baekjoon/문자열/2231.py
@@ -0,0 +1,10 @@
+# 생성자, 분해합 구하기
+n = int(input())
+for i in range(1, n+1):
+    num = sum(map(int, str(i)))
+    num_sum = i+num
+    if num_sum == n:
+        print(i)
+        break
+    if i == n:
+        print(0)
