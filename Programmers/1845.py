4b7b8f7c8585a8690795bd1195c557631910fffa
ijieun <najee0610@naver.com>
2024-02-05 17:58:54 +0900

    solve:1845

---
+++ Programmers/1845.py
@@ -0,0 +1,10 @@
+# 최대 n/2마리. 
+# 그냥 set으로 중복 제거해서 그걸 프린트 하면?
+def solution(nums):
+    answer = 0
+    set_n = len(set(nums))
+    if len(nums)//2>set_n:
+        print(set_n)
+    else:
+        print(round(len(nums)/2))
+    return answer
