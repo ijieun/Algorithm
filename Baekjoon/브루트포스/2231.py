c52d5826cd10f04c709ee26cd96ba54b67659322
ijieun <najee0610@naver.com>
2024-02-12 23:43:44 +0900

    solve: 2231

--- Baekjoon/브루트포스/2231.py
+++ Baekjoon/브루트포스/2231.py
@@ -2,9 +2,8 @@
 n = int(input())
 for i in range(1, n+1):
     num = sum(map(int, str(i)))
-    num_sum = i+num
-    if num_sum == n:
+    if i+num == n:
         print(i)
         break
-    if i == n:
-        print(0)
+else:
+    print(0)
