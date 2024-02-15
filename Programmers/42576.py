6b254766d2cc4aa4d7b3fca03cf3fccbb64b9ade
ijieun <najee0610@naver.com>
2024-02-15 19:08:44 +0900

    solve: 42576

--- Programmers/42576.py
+++ Programmers/42576.py
@@ -1,6 +1,16 @@
 def solution(participant, completion):
     dict = {}
-    
-    answer = list(set(participant) - set(completion))
+    for i in participant:
+        if i not in dict:
+            dict[i] = 1
+        else:
+            dict[i] += 1
+    for j in completion:
+        if j in dict:
+            dict[j] -= 1
+    # 값이 1인것만 출력
+    for key, value in dict.items():
+        if value == 1:
+            answer = key
     return answer
-print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
+print(solution(["mislav", "stanko", "mislav", "ana","jini"], ["stanko", "ana", "mislav"]))
