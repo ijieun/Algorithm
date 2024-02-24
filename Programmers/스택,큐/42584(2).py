ea32c5ddad5f65d4058f85aada77e177b7d118dd
ijieun <najee0610@naver.com>
2024-02-24 23:43:37 +0900

    solve: 42584

---
+++ Programmers/스택,큐/42584(2).py
@@ -0,0 +1,16 @@
+from collections import deque
+def solution(prices):
+    answer = []
+    prices=deque(prices)
+    # 이중 반복문: while과 for 문 사용
+    while prices:
+        cnt = 0
+        price = prices.popleft()
+        for i in prices:
+            cnt +=1
+            if i<price:
+                break
+        answer.append(cnt)
+
+    return answer
+print(solution([1, 2, 3, 2, 3]))
