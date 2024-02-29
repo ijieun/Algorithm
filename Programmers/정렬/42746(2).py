f626e97862bb9c8128be2203616a99ecb459000b
ijieun <najee0610@naver.com>
2024-02-29 11:41:27 +0900

    sovle: 42746

---
+++ Programmers/정렬/42746(2).py
@@ -0,0 +1,19 @@
+from functools import cmp_to_key
+# cmp_to_key 사용!
+
+def compare(x,y):
+    # 나중에 들어온 요소가 앞으로 정렬됨 (뒤에거)
+    if x+y > y+x:
+        return 1
+    # 바뀌지 않음
+    elif x+y == y+x:
+        return 0
+    # 먼저 들어온 요소가 앞으로 정렬됨 (앞에거)
+    else:
+        return -1
+
+def solution(numbers):
+    n = [str(x) for x in numbers]
+    n = sorted(n, key=cmp_to_key(compare),reverse=True)
+    answer = str(int(''.join(n)))
+    return answer
