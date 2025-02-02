6c616985c071b257c4cf45ccbfaf499964ab8754
Jieun Lee <najee0610@naver.com>
2025-02-02 23:20:52 +0900

    완전탐색

---
+++ codetree/nm/nm_bruteforce_test.py
@@ -0,0 +1,14 @@
+import sys
+N, S = map(int, input().split())
+arr = list(map(int, input().split()))
+
+min_value = sys.maxsize
+
+for i in range(N):
+    for j in range(i+1, N):
+        min_value = min(min_value, abs(S-(sum(arr)-arr[i]-arr[j])))
+        # min_value = S-(sum(arr)-arr[i]-arr[j])
+        print(f"S-{(sum(arr))}-{arr[i]}-{arr[j]})")
+
+
+print(min_value)
